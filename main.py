7852e627c566f041427f31a4b24afe5363ab18b4
Ali Hussain <alifed2005@gmail.com>
2019-08-28 21:02:35 +0500

    8/28/2019

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import sqlalchemy
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
 app.run(debug=True)
