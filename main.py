8de1d53a0764a1723741f6c37bf2edebfda51ca0
Ali Hussain <alifed2005@gmail.com>
2019-08-29 20:44:16 +0500

    8/29/2019

--- main.py
+++ main.py
@@ -8,29 +8,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-@app.route("/post")
-def posts():
-    return render_template("post.html")
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 @app.route("/post")
 def post():
-    post = {'title': '', 'author': '', 'date' : '', 'heading' : '', 'subheading' : '', 'content' : '<h1> Test </h1>'}
+    post = {'title': '', 'author': '', 'date': '', 'heading': '',
+            'subheading': '', 'content': '<h1> Test </h1>'}
     return render_template("post.html", post=post)
 
 
