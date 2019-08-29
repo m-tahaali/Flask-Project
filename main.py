02798cf6f1e9a0df7fd684bff2a5363826709b1f
Taha Ali <karatemastertaha@gmail.com>
2019-08-29 19:47:55 +0500

    Converted Templates to use Flask satic code.

--- main.py
+++ main.py
@@ -9,4 +9,19 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
+
+
 app.run(debug=True,port=80,host="0.0.0.0")
