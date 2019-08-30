28770f3abdc376df297c5ed31776b4ab7cc46f8e
Ali Hussain <alifed2005@gmail.com>
2019-08-30 16:32:30 +0500

    8/30/2019

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
-import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 import markupsafe
+import datetime
+
+class post_props():
+    def __init__(self,title="Post",author="Ali or Taha",date=datetime.datetime.now().strftime("%x"),heading="Ali",subheading=None,content=None):
+        self.title=title
+        self.author=author
+        self.date=date
+        self.heading=heading
+        self.subheading=subheading
+        self.content=content
+
+a=post_props(heading="Taha")
 
 app = Flask(__name__)
 
@@ -19,14 +31,10 @@ def contact():
 def about():
     return render_template("about.html")
 
-class post_props():
-    def __init__(self):
 
 @app.route("/post")
 def post():
-    post = {'title': '', 'author': '', 'date': '', 'heading': '', 'subheading': '',
-            'content': markupsafe.Markup('<h1> Test </h1>')}
-    return render_template("post.html", post=post)
+    return render_template("post.html", post=a)
 
 
 app.run(debug=True, port=80, host="0.0.0.0")
